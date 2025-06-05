e84c01933c5552ae012f84a3428f312e05422a5b
Ethan Job <efbascug@gmail.com>
2025-06-06 06:46:37 +0800

    [examples] Added data-test for post-btn. For intercept tutorials

--- app/components/PostButton.jsx
+++ app/components/PostButton.jsx
@@ -13,7 +13,7 @@ export default function PostButton(){
     }
 
     return (
-        <Button onClick={handleClick}>
+        <Button data-test="post-btn" onClick={handleClick}>
             Post Data
         </Button>
     )
