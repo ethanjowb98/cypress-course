25917edf89c3c6f4320ac6f024f57502cdda38db
Ethan Job <efbascug@gmail.com>
2025-06-06 06:43:36 +0800

    [forms] Added data-test in forms page

--- app/forms/page.jsx
+++ app/forms/page.jsx
@@ -40,25 +40,29 @@ export default function FormsPage(){
             <h1 className={styles.header}>Testing Forms</h1>
             <ItemsAccordion items={items} />
             <TextField 
-            className={styles.input} 
-            label="Email" 
-            variant="filled" 
-            value={inputValue}
-            onChange={e => setInputValue(e.target.value)}
+                data-test="email-input"
+                className={styles.input} 
+                label="Email" 
+                variant="filled" 
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
             />
-            <Button onClick={() => {
-                if (!inputValue.includes('.com') ){
-                    setSubMessage(`Invalid email: ${inputValue}!`)
-                } else if (inputValue.length){
-                    setSubMessage(`Successfully subbed: ${inputValue}!`)
-                } else {
-                    setSubMessage('fail!')
+            <Button 
+                data-test="subscribe-btn"
+                onClick={() => {
+                    if (inputValue && !inputValue.includes('.com') ){
+                        setSubMessage(`Invalid email: ${inputValue}!`)
+                    } else if (inputValue.length){
+                        setSubMessage(`Successfully subbed: ${inputValue}!`)
+                    } else {
+                        setSubMessage('fail!')
+                    }
+                    setTimeout(() => {
+                        setSubMessage('')
+                        setInputValue('');
+                    }, 3000)
                 }
-                setTimeout(() => {
-                    setSubMessage('')
-                    setInputValue('');
-                }, 3000)
-            }}>
+            }>
                 Subscribe
             </Button>
             {
