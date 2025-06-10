95227b8ed08d02db77001d30c1efc9edd0c558d0
Ethan Job <efbascug@gmail.com>
2025-06-11 02:23:31 +0800

    [cicd] Adding github actions to the project

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // For test reporting
     },
-  },
-});
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
+})
+
